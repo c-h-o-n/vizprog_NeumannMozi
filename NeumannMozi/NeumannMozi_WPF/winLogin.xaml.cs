dbd85bf4a7f444aa249334de2ef984c76897a6e8
Chon <balint.ducsai@gmail.com>
2020-04-25 20:02:05 +0200

    Make database synchronized

--- NeumannMozi/NeumannMozi_WPF/winLogin.xaml.cs
+++ NeumannMozi/NeumannMozi_WPF/winLogin.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,18 @@ namespace NeumannMozi_WPF {
     /// </summary>
     public partial class winLogin : Window {
         public winLogin() {
+            SetDataDirectory();
             InitializeComponent();
         }
+
+        #region Database
+        private void SetDataDirectory() {
+            var dir = Directory.GetParent(Directory.GetCurrentDirectory());
+            dir = Directory.GetParent(dir.ToString());
+            dir = Directory.GetParent(dir.ToString());
+            //
+            AppDomain.CurrentDomain.SetData("DataDirectory", dir.ToString());
+        }
+        #endregion
     }
 }
