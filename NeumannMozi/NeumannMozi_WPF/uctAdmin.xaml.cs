7e61a8ea3507701849c44d07798a13b09f2a3186
Chon <balint.ducsai@gmail.com>
2020-05-06 15:01:04 +0200

    Admin screen[10%] + Add screen[90%]

---
+++ NeumannMozi/NeumannMozi_WPF/uctAdmin.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NeumannMozi_WPF {
+    /// <summary>
+    /// Interaction logic for uctAdmin.xaml
+    /// </summary>
+    public partial class uctAdmin : UserControl {
+        public uctAdmin() {
+            InitializeComponent();
+        }
+
+        private void btnAddMovie_Click(object sender, RoutedEventArgs e) {
+            winAddFilm winAddFilm = new winAddFilm();
+            winAddFilm.Show();
+        }
+    }
+}
